54d5904caec0def23100f0cbe7e80dc9ade8029a
teagueStockwell <tsAppDevelopment@gmail.com>
2022-08-09 23:28:26 -0700

    Update test server

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
-const dist = require('./dist')
-
+const dist = require('./dist');
 const app = express();
 
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json('ok');
+  res.status(200).json(Date.now());
 });
 
 app.get('/', async (req, res) => {
   const { url } = req.query;
+
   if (!url || typeof url !== 'string') {
     res.status(400).json({ error: 'url is required' });
     return;
   }
 
-  res
-    .status(200)
-    .json(await dist.scrapeMeta({ url, type: 'headed', wait: 1000 }));
+  try {
+    const data = await dist.scrapeMeta({ url, type: 'headed', wait: 1000 });
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 app.listen(process.env.PORT);
-
-module.exports = app;
+console.log(`listening on ${process.env.PORT} at ${new Date().toISOString()}`);
