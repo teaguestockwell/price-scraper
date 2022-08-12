4fa955a30aa83df50fadd39241023f29651c9813
teagueStockwell <tsAppDevelopment@gmail.com>
2022-08-12 00:54:27 -0700

    Replace puppeteer with playwright

--- src/common/clients.ts
+++ src/common/clients.ts
@@ -1,23 +1,7 @@
-import { Browser } from 'puppeteer-extra-plugin/dist/puppeteer';
-import pup from 'puppeteer-extra';
-// https://github.com/berstend/puppeteer-extra/issues/93#issuecomment-563159177
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import s0 from 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
-import s1 from 'puppeteer-extra-plugin-stealth/evasions/chrome.csi';
-import s2 from 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
-import s3 from 'puppeteer-extra-plugin-stealth/evasions/defaultArgs';
-import s4 from 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow';
-import s5 from 'puppeteer-extra-plugin-stealth/evasions/media.codecs';
-import s6 from 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency';
-import s7 from 'puppeteer-extra-plugin-stealth/evasions/navigator.languages';
-import s8 from 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions';
-import s9 from 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins';
-import s10 from 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor';
-import s11 from 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver';
-import s12 from 'puppeteer-extra-plugin-stealth/evasions/sourceurl';
-import s13 from 'puppeteer-extra-plugin-stealth/evasions/user-agent-override';
-import s14 from 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor';
-import s15 from 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions';
+import { LaunchOptions, Browser } from 'playwright';
+import { chromium as pw } from 'playwright-extra';
+import { XMLParser } from 'fast-xml-parser';
+import stealth from 'puppeteer-extra-plugin-stealth';
 
 import metaScraper from 'metascraper';
 import shopping from '@samirrayani/metascraper-shopping';
@@ -26,33 +10,8 @@ import metaImage from 'metascraper-image';
 import metaUrl from 'metascraper-url';
 
 import axios from 'axios';
-import { XMLParser } from 'fast-xml-parser';
 
-const plugins = [
-  StealthPlugin(),
-  s0(),
-  s1(),
-  s2(),
-  s3(),
-  s4(),
-  s5(),
-  s6(),
-  s7(),
-  s8(),
-  s9(),
-  s10(),
-  s11(),
-  s12(),
-  s13(),
-  s14(),
-  s15(),
-];
-
-const applyPlugins = async (browser: Browser) => {
-  for (const plugin of plugins) {
-    await plugin.onBrowser(browser);
-  }
-};
+pw.use(stealth());
 
 const parser = new XMLParser({
   unpairedTags: ['link', 'meta'],
@@ -68,55 +27,36 @@ let headed: Browser | undefined;
 let headless: Browser | undefined;
 
 const launchArgs = (() => {
-  const options = process.env.PUPPETEER_LAUNCH_ARGS;
-  const chromePath = process.env.CHROME_PATH;
-  const defaultArgs = {
-    args: [
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--no-first-run',
-      '--no-sandbox',
-      '--no-zygote',
-      '--single-process',
-    ],
-    env: { DISPLAY: ':10.0' },
-    executablePath: chromePath ? chromePath : undefined,
-  };
-  if (options) {
+  const defaultOps: LaunchOptions = {};
+  let ops = process.env.PLAYWRIGHT_ARGS;
+  if (ops) {
     try {
-      const args = JSON.parse(options);
-      console.log(`Puppeteer launch args: ${JSON.stringify(args)}`);
-      return args;
+      ops = JSON.parse(ops);
     } catch {
-      console.warn('failed to parse PUPPETEER_LAUNCH_ARGS');
+      ops = undefined;
     }
   }
-  console.log(`Puppeteer launch args: ${JSON.stringify(defaultArgs, null, 2)}`);
-  return defaultArgs;
+  const using = ops ?? defaultOps;
+  console.log('using browser launch args', JSON.stringify(using, null, 2));
+  return using as LaunchOptions;
 })();
 
 const getHeadedBrowser = async () => {
   if (!headed) {
-    headed = await pup.launch({ headless: false, ...launchArgs });
-    applyPlugins(headed);
+    headed = await pw.launch({ headless: false, ...launchArgs });
   }
   return headed;
 };
 
 const getHeadlessBrowser = async () => {
   if (!headless) {
-    headless = await pup.launch({ headless: true, ...launchArgs });
-    applyPlugins(headless);
+    headless = await pw.launch({ headless: true, ...launchArgs });
   }
   return headless;
 };
 
 export const getPage = async (browser: Browser) => {
   const page = await browser.newPage();
-  for (const plugin of plugins) {
-    await plugin.onPageCreated(page);
-  }
   return page;
 };
 
